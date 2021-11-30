04fece4ec992bfc8e960c1a18b4ffe7fe03609f8
김정호 <cloud0406@naver.com>
2021-11-30 20:50:09 +0900

    practice : JS의 Math 정리
    * Math 기능 정리
    * 절댓값, 최댓값, 최솟값, 거듭제곱, 제곱근, 반올림, 올림&내림, 난수

---
+++ JS의 기능들/Math 정리/index.js
@@ -0,0 +1,56 @@
+// 절댓값 (Absolute Number)
+console.log(Math.abs(-10));
+console.log(Math.abs(10));
+
+console.log('');
+
+// 최댓값 (Maximum) : 괄호 안의 수들 중 가장 큰 값 리턴
+console.log(Math.max(2, -1, 4, 5, 0));
+
+console.log('');
+
+// 최솟값 (Minimum) : 괄호 안의 수들 중 가장 작은 값 리턴
+console.log(Math.min(2, -1, 4, 5, 0));
+
+console.log('');
+
+// 거듭제곱 (Exponentiation) : Math.pow(x, y) -> x의 y승
+console.log(Math.pow(2, 3));
+console.log(Math.pow(5, 2));
+
+console.log('');
+
+// 제곱근 (Square Root)
+console.log(Math.sqrt(25));
+console.log(Math.sqrt(49));
+
+console.log('');
+
+// 반올림 (Round)
+console.log(Math.round(2.3));
+console.log(Math.round(2.4));
+console.log(Math.round(2.49));
+console.log(Math.round(2.5));
+console.log(Math.round(2.6));
+
+console.log('');
+
+// 버림과 올림 (Floor and Ceil)
+// Math.floor(x) : 버림 , Math.ceil(x) : 올림 
+console.log(Math.floor(2.4));
+console.log(Math.floor(2.49));
+console.log(Math.floor(2.8));
+console.log('-');
+console.log(Math.ceil(2.4));
+console.log(Math.ceil(2.49));
+console.log(Math.ceil(2.8));
+
+console.log('');
+
+// 난수 (Random) : Math.random -> 0 이상 1 미만의 값이 랜덤으로 리턴
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+
+console.log('');
