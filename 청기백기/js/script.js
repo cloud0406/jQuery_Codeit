348da7063e26b82e0e9fb8a7462ba2bc79f8537e
김정호 <cloud0406@naver.com>
2021-12-30 21:04:31 +0900

    refactor : 깃발 올리는 함수 추가
    * 기존에 깃발이 내려가도 다시 안올라가는 문제 수정
    * setTimeout 함수 이용해 버튼 누른 후 1초 후에 깃발이 올라가도록 실행

--- 청기백기/js/script.js
+++ 청기백기/js/script.js
@@ -6,12 +6,21 @@ $('#btn3').on('click',blueDotDown);
 // 따라서 버튼을 누르면 해당 버튼에 맞는 요소들에 down 클래스를 추가하여 회전시키도록 함
 function blueDown() {
   $('.blue').addClass('down');
+  // 'setTimeout'함수는 동작을 몇초 후에 실행되도록 사용자가 설정할 수 있음(기본적으로 내장되어있는 함수)
+  setTimeout(reset,1000); // 1000ms = 1초,  후에 아래에 만들어둔 'reset'함수 실행하도록 설정
 }
 
 function whiteDown() {
   $('.white').addClass('down');
+  setTimeout(reset,1000);
 }
 
 function blueDotDown() {
   $('.blue.dot').addClass('down');
+  setTimeout(reset,1000);
+}
+
+// 버튼에 해당하는 깃발을 내린 후 다시 올려주는 함수
+function reset() {
+  $('.flag').removeClass('down');
 }
