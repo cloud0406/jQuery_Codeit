b07176d41473a95ae27966310bb8aaab3777dae1
김정호 <cloud0406@naver.com>
2022-01-16 02:22:36 +0900

    refactor : 섹션에 애니메이션 효과 적용
    * 스크롤을 내리며 각 섹션에 들어가면 투명도 변경하여 보이게끔 수정
    * about 섹션에 inner-bar 텍스트에 맞게 채워지도록 수정

--- 포트폴리오 사이트 만들기/js/script.js
+++ 포트폴리오 사이트 만들기/js/script.js
@@ -1,9 +1,23 @@
 function scrollHandler() {
   if ($(window).scrollTop() >= $('.about').position().top) { // 현재 브라우저의 스크롤 맨 윗부분이 about 섹션 밑으로 갈 경우 오른쪽 위의 메뉴바의 색깔을 변경해줌 
     $('.menu-right button').css('color','#4a4a4a');
+
+    $('.skill').each(function(){ // each문이 모든 skill 클래스를 돌면서
+      let skill = $(this); 
+      let percentage = skill.find('.percentage').text(); // 해당 skill 클래스의 percentage 텍스트를 가져와서
+      skill.find('.inner-bar').animate({width: percentage},1200); // inner-bar 클래스에 애니메이션 효과로 적용
+    })
   } else { // 다시 윗부분으로 스크롤 이동할 경우 원래 색인 하얀색으로 변경
     $('.menu-right button').css('color','white');
   }
+
+
+  // 현재 스크롤 위치가 해당 섹션보다 아래에 있으면 vertical-center를 애니메이션 효과 적용시킴
+  $('section').each(function() { // 모든 섹션에 대하여
+    if ($(window).scrollTop() >= $(this).position().top){ // 현재 스크롤 윗부분이 각 섹션의 윗부분보다 아래로 내려오게 될 경우 
+      $(this).find('.vertical-center').animate({opacity:'1'},800); // 해당 섹션의 'vertical-center' 클래스의 투명도를 올려서 화면에 보이게끔 함
+    }
+  })
 }
 
 // 스크롤할때마다 위의 함수 실행
@@ -22,3 +36,4 @@ $('.menu-right button').on('click',function() { // 메뉴바 버튼 클릭시 �
     $('html, body').animate({scrollTop: $('.contact').position().top},1000); // 스크롤 맨 윗부분이 contact 클래스의 섹션으로 1초 동안 이동
   }
 })
+
